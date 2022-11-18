bb647583f541b3b95fe7d79a733eb8c1a87d0957
Hussein <aisackhussein@gmail.com>
2022-11-18 22:45:43 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Muktar Hussein on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
